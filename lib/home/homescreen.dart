63fea570f744f72d182e23b997eaef58faa4257d
meralalfawi <meralalfawi@gmail.com>
2023-10-04 19:48:32 +0300

    initial project . added empty home screen .

---
+++ lib/home/homescreen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class HomeScreen extends StatelessWidget {
+static const String routeName = 'homescreen';
+
+  @override
+  Widget build(BuildContext context) {
+    return ;
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:todo_app/home/homescreen.dart';
+
+void main() {
+  runApp( MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+   return MaterialApp(
+     initialRoute:  HomeScreen.routeName,
+     routes: {
+       HomeScreen.routeName : (context) => HomeScreen(),
+     },
+   );
+  }}
